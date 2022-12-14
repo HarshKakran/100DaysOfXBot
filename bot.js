10d3ac0c484ee20165f2ee405c4a38a54c698ada
HarshKakran <harshkakran05@gmail.com>
2022-12-14 17:25:55 +0530

    Clubbed fav and retweet action, changed the process to retweet other hashtags

--- bot.js
+++ bot.js
@@ -10,54 +10,35 @@ let Twitter = new twit({
                         access_token_secret: process.env.ACCESS_TOKEN_SECRET, 
                     })
 
-let retweet  = () => {
+let retweet_and_fav  = (hashtags) => {
     let params = {
-        q: '#100DaysOfX, #100daysofx, #100DaysOfCode, #100DaysOfHealth, #100DaysOfFitness',
+        q: hashtags,
         result_type: 'recent',
     }
-
+    console.log(`Let's get some tweets!`)
     Twitter.get('search/tweets', params, (err, data) => {
         // if no error
         if(!err){
             // grab ID of tweet to retweet
-            let retweets = data.statuses;
-            for(let i=0; i<retweets.length; i++){    
-                let retweetId = retweets[i].id_str;
+            console.log(`No Error in searching...`)
 
+            let tweets = data.statuses;
+            console.log(`Tweets: ${tweets.length}`)
+            for(let i=0; i<tweets.length; i++){    
+                let tweetId = tweets[i].id_str;
+
+                console.log(`Time to retweet ${tweetId}`);
                 Twitter.post('statuses/retweet/:id', {
-                    id: retweetId
+                    id: tweetId
                 }, (e, response) => {
                     if(response)
-                        console.log(`Retweeted!!`);
+                    console.log(`Retweeted!!`);
                     if(e)
-                        console.log(`Something went wrong while RETWEETING... Error is: ${e.message}`);
+                    console.log(`Something went wrong while RETWEETING... Error is: ${e.message}`);
                 });
-            }
-        }
-
-        // if unable to search
-        else {
-            console.log(`Something went wrong while searching... Error is: ${err.message}`);
-        }
-    });
-}
-
-let favorite  = () => {
-    let params = {
-        q: '#100DaysOfX, #100daysofx, #100DaysOfCode, #100DaysOfHealth, #100DaysOfFitness',
-        result_type: 'recent',
-    }
-
-    Twitter.get('search/tweets', params, (err, data) => {
-        // if no error
-        if(!err){
-            // grab ID of tweet to favorite
-            let favorites = data.statuses;
-            for(let i=0; i<favorites.length; i++){    
-                let favoriteId = favorites[i].id_str;
-
+                console.log(`Time to favorite ${tweetId}`);
                 Twitter.post('favorites/create', {
-                    id: favoriteId
+                    id: tweetId
                 }, (e, response) => {
                     if(response)
                         console.log(`FAVORITED!!`);
@@ -74,10 +55,44 @@ let favorite  = () => {
     });
 }
 
+// let favorite  = () => {
+//     let params = {
+//         q: '#100DaysOfX, #100daysofx, #100DaysOfCode, #100DaysOfHealth, #100DaysOfFitness',
+//         result_type: 'recent',
+//     }
 
-// grab and retweet as soo as program is running...
-retweet()
-favorite()
-// retweet in every 50 minutes
-setInterval(retweet, 1000 * 60);
-setInterval(favorite, 1000 * 60);
+//     Twitter.get('search/tweets', params, (err, data) => {
+//         // if no error
+//         if(!err){
+//             // grab ID of tweet to favorite
+//             let favorites = data.statuses;
+//             for(let i=0; i<favorites.length; i++){    
+//                 let favoriteId = favorites[i].id_str;
+
+//                 Twitter.post('favorites/create', {
+//                     id: favoriteId
+//                 }, (e, response) => {
+//                     if(response)
+//                         console.log(`FAVORITED!!`);
+//                     if(e)
+//                         console.log(`Cannot be FAVORITE... Error is: ${e.message}`);
+//                 });
+//             }
+//         }
+
+//         // if unable to search
+//         else {
+//             console.log(`Something went wrong while searching... Error is: ${err.message}`);
+//         }
+//     });
+// }
+
+console.log(`Starting #100DaysOfX`);
+retweet_and_fav('#100DaysOfX, #100daysofx');
+
+console.log(`Starting #100DaysOfCode`)
+setTimeout(retweet_and_fav, 1000*60, '#100DaysOfCode, #100daysofcodingchallenge');
+
+
+console.log(`Starting #100DaysOfHealth and Fitness`)
+setTimeout(retweet_and_fav, 1000*60, '#100DaysOfHealth, #100DaysOfFitness');
